df783762cbe1794511eb695538eee95d7b4882b6
Luis Castro <castro-1117@hotmail.com>
2023-02-14 18:39:55 -0500

    Organización de código y comentarios

--- js/codigo.js
+++ js/codigo.js
@@ -1,19 +1,3 @@
-// Declaración de funciones
-function choose(option){
-    if(option == 1){
-        return "👊";
-    } else if(option == 2){
-        return "🖐";
-    } else if(option == 3){
-        return "✌";
-    } else{
-        return "You chose the wrong option, try again.";
-    }
-}
-function random(min, max){
-    return Math.floor(Math.random() * (max - min + 1) + min);
-}
-
 // Declaración de variables
 let player = 0;
 let pc = 0;
@@ -21,18 +5,18 @@ let playerCounter = 0;
 let pcCounter = 0;
 let tieCounter = 0;
 
-// Mejor de 3
+// Comprobación de combate
 while (playerCounter < 3 && pcCounter < 3) {
-    pc = random (1, 3); // Invocación de función random con parametro min y max
+    // Invocación de 'random' con parametro min y max
+    pc = random (1, 3); 
 
     // Instrucciones
     player = prompt("Choose: 1 for stone 👊, 2 for paper 🖐 or 3 for scissor ✌.");
 
-    // Mensaje de elección
-    // En esta parte, invocamos la función "choose" pero sustituimos el parametro "option" con el parametro "player" y "pc"
+    // Elección de cada jugador invocando 'choose'
     alert("You chose " + choose(player) + ' and The PC chose ' + choose(pc));
 
-    // Combate
+    // Comparación de resultados
     if((player == 1 && pc == 3) || (player == 2 && pc == 1) || (player == 3 && pc == 2)){
         alert("You Win!");
         playerCounter ++;
@@ -45,8 +29,28 @@ while (playerCounter < 3 && pcCounter < 3) {
     }
 }
 
+// Opciones de usuario
+function choose(option){
+    if(option == 1){
+        return "👊";
+    } else if(option == 2){
+        return "🖐";
+    } else if(option == 3){
+        return "✌";
+    } else{
+        return "You chose the wrong option, try again.";
+    }
+}
+
+// Generar un numero aleatorio entre un minimo y un maximo
+function random(min, max){
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+// indicar el ganador de la partida
 alert("You win " + playerCounter + " times, the PC win " + pcCounter + " times and the number of ties is " + tieCounter);
 
+// indicar el ganador del encuentro
 if (playerCounter > pcCounter) {
     alert("🥳 You are the Winner! 🎉");
 }
